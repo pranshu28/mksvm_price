f99caa84af1f2806b0d26a03189837b43cef99db
Yash Pratyush Sinha <yps1112@gmail.com>
2016-10-20 19:51:33 +0530

    Updated RBF

--- kernels/multi_kernel.py
+++ kernels/multi_kernel.py
@@ -2,7 +2,7 @@ from sklearn.svm import SVR
 import numpy as np
 import math
 def rbf(x1,x2,gamma=1.0):
-	return (np.linalg.norm(x1-x2))*(-1.0*gamma)
+	return math.exp((np.linalg.norm(x1-x2))*(-1.0*gamma))
 def lin(x1,x2,offset=0):
 	return x1.dot(x2)+offset
 def poly(x1,x2,power=3,offset=0):
