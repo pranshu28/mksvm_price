c6618b5ed4146594b895137bbd5d925807b94057
Pranshu Malviya <pranshumalviya2@users.noreply.github.com>
2016-10-24 01:43:19 +0530

    Update multi_kernel.py

--- kernels/multi_kernel.py
+++ kernels/multi_kernel.py
@@ -1,6 +1,11 @@
+
 from sklearn.svm import SVR
 import numpy as np
+import pandas as pd
 import math
+import matplotlib.pyplot as plt
+import itertools
+
 
 def rbf(gamma=1.0):
 	def rbf_fun(x1,x2):
@@ -12,7 +17,7 @@ def lin(offset=0):
 		return x1.dot(x2.transpose())+offset
 	return lin_fun
 
-def poly(power=1,offset=0):
+def poly(power=2,offset=0):
 	def poly_fun(x1,x2):
 		return pow(x1.dot(x2.transpose())+offset,power)
 	return poly_fun
@@ -44,7 +49,7 @@ def beta_finder(x,y,kernel_list):
 
 def multi_kernel_maker(x,y,kernel_list):
 	betas=[float(b) for b in beta_finder(x,y,kernel_list)]
-	print(betas)
+	#print "	",betas
 	def multi_kernal(x1,x2):
 		mat=np.zeros((x1.shape[0],x2.shape[0]))
 		for a in range(x1.shape[0]):
@@ -53,15 +58,27 @@ def multi_kernel_maker(x,y,kernel_list):
 		return mat
 	return multi_kernal
 
-x=np.matrix([[1,2],[2,4],[3,6],[4,8],[5,10]])
-y=np.matrix([[2],[4],[6],[8],[10]])
-a=multi_kernel_maker(x,y,[lin()])
-svr=SVR(C=1.5,kernel=a)
-c=svr.fit(x,np.squeeze(np.asarray(y)))
+#Input all the data
+x_gdp = pd.read_csv("Cleaned.csv",header=None).as_matrix()
+x_train = x_gdp[:-3,:]
+x_test = x_gdp[-3:,:]
+y_comm = pd.read_csv("Cleaned_prices.csv",header=None).as_matrix()
+y_train = y_comm[:-3,:]
+y_act = y_comm[-3:,:]
+kernels = [lin(),poly(),poly(3),rbf(),sig()]
+multi_kernels = [mult for mult in itertools.combinations(kernels, 2)]
 
-# svr predict needs same number of samples as used in traing.. need to fix that
-# Fixed it :)
-x_new=np.matrix([[6,10]])
-y_new=svr.predict(x_new)
-y_test=svr.predict(x)
-print c,y_new,y_test
+# x=np.matrix([[1,2],[2,4],[3,6],[4,8],[5,10]])
+# y=np.matrix([[2],[4],[6],[8],[10]])
+
+#Run for each pair of kernel
+for i in range(2):#y_train.shape[1]):
+	print "Error (past 3 years) in Price of commodity ",i+1
+	for k_list in multi_kernels:
+		y=[[t] for t in y_train[:,i]]
+		kernel=multi_kernel_maker(x_train,y,k_list)
+		svr=SVR(C=1.0,kernel=kernel)
+		c=svr.fit(x_train,np.squeeze(y))
+		y_test=svr.predict(x_test)
+		error = y_act[:,i]-y_test
+		print "	  		",error,np.std(error)
